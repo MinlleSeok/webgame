c4de121456ca453dc0c721382b9a1d0e70022bd9
hotheating <>
2020-01-24 23:10:52 +0900

    Updates

---
+++ _next/static/webpack/static/development/pages/index.js.22d7d4b1e3636ecd5718.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./pages/index.module.css":
+/*!********************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-oneOf-2-1!./node_modules/postcss-loader/src??__nextjs_postcss!./pages/index.module.css ***!
+  \********************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(true);
+// Module
+exports.push([module.i, ".index_home__3lYMY {\r\n    background: red;\r\n  \r\n}\r\n\r\n.index_home__3lYMY > form {\r\n    display: flex;\r\n    flex-direction: column;\r\n}\r\n\r\n@media (max-width: 600px) {\r\n    .index_home__3lYMY {\r\n        background: blue;\r\n    }\r\n}", "",{"version":3,"sources":["index.module.css"],"names":[],"mappings":"AAAA;IACI,eAAe;;AAEnB;;AAEA;IACI,aAAa;IACb,sBAAsB;AAC1B;;AAEA;IACI;QACI,gBAAgB;IACpB;AACJ","file":"index.module.css","sourcesContent":[".home {\r\n    background: red;\r\n  \r\n}\r\n\r\n.home > form {\r\n    display: flex;\r\n    flex-direction: column;\r\n}\r\n\r\n@media (max-width: 600px) {\r\n    .home {\r\n        background: blue;\r\n    }\r\n}"]}]);
+// Exports
+exports.locals = {
+	"home": "index_home__3lYMY"
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.22d7d4b1e3636ecd5718.hot-update.js.map
