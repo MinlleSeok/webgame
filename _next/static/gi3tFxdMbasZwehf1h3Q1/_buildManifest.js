1cadf2ef8775f6f410302d009b81374c374234ea
hotheating <hotheating@gmail.com>
2020-01-24 20:05:32 +0900

    Updates

---
+++ _next/static/gi3tFxdMbasZwehf1h3Q1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/index":[a]}}("static\u002Fcss\u002F7006106de0f9980f0bef.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
