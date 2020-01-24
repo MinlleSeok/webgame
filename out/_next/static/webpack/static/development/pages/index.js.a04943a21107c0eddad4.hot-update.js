40d4702a1d4e388bfe3e8f6b7de61ef7a9c65b28
hotheating <>
2020-01-24 21:00:49 +0900

    init commit

---
+++ out/_next/static/webpack/static/development/pages/index.js.a04943a21107c0eddad4.hot-update.js
@@ -0,0 +1,87 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _index_module_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.module.css */ "./pages/index.module.css");
+/* harmony import */ var _index_module_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_index_module_css__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/router */ "./node_modules/next/dist/client/router.js");
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_2__);
+var _jsxFileName = "C:\\Users\\Administrator\\Documents\\webgame\\pages\\index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+function HomePage() {
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(),
+      state = _useState[0],
+      setState = _useState[1];
+
+  var action = function action(e) {
+    e.preventDefault();
+    next_router__WEBPACK_IMPORTED_MODULE_2___default.a.push({
+      pathname: '/about',
+      query: {
+        name: state
+      }
+    });
+  };
+
+  return __jsx("div", {
+    className: _index_module_css__WEBPACK_IMPORTED_MODULE_1___default.a.home,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, __jsx("form", {
+    onSubmit: action,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, __jsx("label", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "\uB2F9\uC2E0\uC758 \uBCC4\uBA85\uC744 \uC785\uB825\uD558\uC0C8\uC624"), __jsx("input", {
+    type: "text",
+    value: state,
+    onChange: function onChange(e) {
+      return setState(e.target.value);
+    },
+    required: true,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }), __jsx("input", {
+    type: "submit",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  })));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (HomePage);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.a04943a21107c0eddad4.hot-update.js.map
---
+++ out/_next/static/webpack/static/development/pages/index.js.f211a72d2f362bb8f0d8.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/postcss-loader/src/index.js?!./pages/index.module.css":
+/*!********************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-oneOf-2-1!./node_modules/postcss-loader/src??__nextjs_postcss!./pages/index.module.css ***!
+  \********************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js")(true);
+// Module
+exports.push([module.i, "#index_home__3lYMY {\r\n    background: red;\r\n    display: flex;\r\n    flex-direction: column;\r\n}\r\n\r\n@media (max-width: 600px) {\r\n    #index_home__3lYMY {\r\n        background: blue;\r\n    }\r\n}", "",{"version":3,"sources":["index.module.css"],"names":[],"mappings":"AAAA;IACI,eAAe;IACf,aAAa;IACb,sBAAsB;AAC1B;;AAEA;IACI;QACI,gBAAgB;IACpB;AACJ","file":"index.module.css","sourcesContent":["#home {\r\n    background: red;\r\n    display: flex;\r\n    flex-direction: column;\r\n}\r\n\r\n@media (max-width: 600px) {\r\n    #home {\r\n        background: blue;\r\n    }\r\n}"]}]);
+// Exports
+exports.locals = {
+	"home": "index_home__3lYMY"
+};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.f211a72d2f362bb8f0d8.hot-update.js.map
